2197d732e1eee2a36b8c32e7ffc025bcf769fb6e
Anta <antambaye261971@gmail.com>
2021-12-30 02:49:37 +0000

    webpack et supabase

--- src/app.js
+++ src/app.js
@@ -25,5 +25,131 @@ const signUp = e => {
     else{
         alert("Ooopppssss... Deja creer");
     }
-    e.preventDefault();
+  
 }
+   /////////////////////////////////////////////////// 
+
+// RECUPERATION DES INFORMAIONS DU FORMULAIRE
+const SecondForm = document.getElementById("form2")
+const ideeForm = document.querySelector("form")
+const inputPrenom = document.querySelector("input#prenom")
+const inputNom = document.querySelector("input#nom")
+const inputOption = document.querySelector("select#niveau")
+const inputBio = document.querySelector("textarea#bio")
+const BtnAdd =document.getElementById("btnAjout")
+const Valider = document.getElementById("valider")
+
+
+Valider.addEventListener("click", (event) => {
+  event.preventDefault()
+
+
+  // Récupération des informations saisies
+  const prenomSaisi = inputPrenom.value
+  const nomSaisi = inputNom.value
+  const optionSaisi = inputOption.value
+  const bioSaisi = inputBio.value
+
+  
+
+  // mettre les informations sous forme
+  const nouvelleIdee = {
+    prenom: prenomSaisi,
+    nom:nomSaisi,
+    niveau:optionSaisi,
+    bio: bioSaisi,
+    
+  }
+  
+
+  //ENVOYER LES DONNEES VERS SUPABASE
+  fetch(API_URL, {
+    method: "POST",
+    headers: {
+      apikey: API_KEY,
+      "Content-Type": "application/json",
+      Prefer:"return=representation",
+    },
+    body: JSON.stringify(nouvelleIdee),
+  })
+  .then((response) => response.json())
+    
+   
+      
+    
+       // on vide les champs
+       inputPrenom.value = ""
+       inputNom.value = ""
+       inputOption.value = ""
+       inputBio.value = ""
+
+  
+})
+
+
+
+
+const creerUneCarte = (idee) => {
+  const idButtonValider ="btn_valider-" +idee.id
+  const idButtonRefuser ="btn_refuser-" +idee.id
+    const idCardIdee= "numero_card" +idee.id
+     
+    
+   
+     SecondForm.insertAdjacentHTML(
+         "afterbegin",
+         `<div class="card " id="${idCardIdee}" style="border: 5px solid black">
+         <div >
+         <img class="card-img-left" alt="" src="./images/undraw_female.svg" width="500px" height="200px">
+         </div>
+         <div class="card-body">
+           <h5 class="card-title">${inputPrenom.value} ${inputNom.value}</h5>
+           <p class="card-text">${inputOption.value}</p>
+           <p class="card-text">${inputBio.value}</p>
+
+         </div>
+         <div class ="d-flex justify-content-between">
+         <i class="fas fa-trash-alt"
+      id="${idButtonValider}"  
+      style="font-size:2rem">
+      
+      </i>
+
+      <i class="fas fa-edit
+      id="${idButtonRefuser}"  
+      style="font-size:2rem; color:#ce0033">
+      </i>
+      </div>
+         
+       </div>
+     </div><br>`
+     )
+   
+   }
+
+   
+   BtnAdd.addEventListener("click", (e) => {
+       e.preventDefault();
+      
+      const prenomSaisi = inputPrenom.value
+        const nomSaisi = inputNom.value
+        const optionSaisi = inputOption.value
+        const bioSaisi = inputBio.value
+       
+       
+        const Apprenant = {
+        prenom:prenomSaisi,
+        nom:nomSaisi,
+        niveau:optionSaisi,
+        bio: bioSaisi,
+        
+                           }
+      
+      creerUneCarte(Apprenant);
+   
+      
+
+
+ 
+      
+   })
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 module.exports = {
   // Le fichier qui sert de point d'entrée et qui importe les différentes dépendances de l'application
-  entry: "./dist/app.js",
+  entry: "./src/app.js",
   // Le dossier et nom du fichier qui sera généré par Webpack après le build
   // Webpack va donc intégrer l'ensemble des modules (dépendances) nécessaires dans un seul fichier dist/app.js
   output: {
